0ba7284efdfcc8569984081c8ec25068c08b3190
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-09-08 01:16:33 +0100

    first update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -12,6 +12,7 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    <GestureHandlerRootView>
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
@@ -20,7 +21,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -41,5 +41,6 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
+    </GestureHandlerRootView>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,161 @@
 
 
-import { View, SafeAreaView, Image, ImageBackground, ScrollView, Pressable, TouchableOpacity, ActivityIndicator, StyleSheet, StatusBar, Text } from "react-native";
+import { View, SafeAreaView, Image, ImageBackground, ScrollView, Pressable, TouchableOpacity, ActivityIndicator, StyleSheet, StatusBar, Text, FlatList, KeyboardAvoidingView, Platform } from "react-native";
 import React, {useState} from "react";
+import { TextInput } from "react-native-gesture-handler";
 
 const localImage = require('@/assets/images/error-404.png')
 
-const DATA = [
-  { id: "1", title: "FlatList Item 1" },
-  { id: "2", title: "FlatList Item 2" },
-  { id: "3", title: "FlatList Item 3" },
+const users = [
+  { id: "1", name: "Abdul" },
+  { id: "2", name: "Maria" },
+  { id: "3", name: "James" },
+  { id: "4", name: "Sophia" },
+  { id: "5", name: "Daniel" },
 ];
 
 export default function HomeScreen() {
   const [count, setCount] = useState(0)
   const [loading, setLoading] = useState(false)
   const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [age, setAge] = useState('')
+
+
   const [enabled, setEnabled] = useState(false)
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle={'dark-content'}/>
-    <Text style={styles.heading}>
-      Welcome to RN
-    </Text>
-    <View style={styles.card}>
-      <Text>
-        Hello In this card
-      </Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#f1f5f9' }}>
+    <ScrollView contentContainerStyle={{ padding: 20 }}>
+      <StatusBar barStyle={'light-content'} />
 
-    <View style={[styles.card, styles.alt]}>
-      <Text numberOfLines={1}>
-        Hello In this card Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quasi placeat? Illo libero nostrum molestiae aliquid totam facere amet provident.
-      </Text>
-    </View>
+      <Text style={styles.heading}>Welcome to RN</Text>
 
-    <Image style={styles.local} source={{uri: "https://plus.unsplash.com/premium_photo-1753982281836-4a7d5cb5792f?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} resizeMode="cover"/>
+      <View style={styles.card}>
+        <Text>Hello In this card</Text>
+      </View>
 
-    <ImageBackground source={localImage} style={styles.backgroundStyle}>
-      <View style={styles.overlay}>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, iste?
+      <View style={[styles.card, styles.alt]}>
+        <Text numberOfLines={1}>
+          Hello In this card Lorem ipsum dolor sit amet consectetur...
         </Text>
       </View>
-    </ImageBackground>
-    </SafeAreaView>
+
+      <Image
+        style={styles.local}
+        source={{
+          uri: 'https://plus.unsplash.com/premium_photo-1753982281836-4a7d5cb5792f?...',
+        }}
+        resizeMode="cover"
+      />
+
+      <ImageBackground source={localImage} style={styles.backgroundStyle}>
+        <View style={styles.overlay}>
+          <Text style={{ color: '#fff' }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, iste?
+          </Text>
+        </View>
+      </ImageBackground>
+
+      {[...Array(30)].map((_, i) => (
+        <Text key={i}>Item {i + 1}</Text>
+      ))}
+
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
+
+      <Pressable
+        onPress={() => alert('Clicked')}
+        style={({ pressed }) => [
+          {
+            padding: 15,
+            margin: 20,
+            borderRadius: 8,
+            backgroundColor: pressed ? '#80deea' : '#000',
+          },
+        ]}>
+        <Text style={{ color: 'white', fontSize: 18, textAlign: 'center' }}>
+          Click me
+        </Text>
+      </Pressable>
+
+      <KeyboardAvoidingView
+      style={{flex: 1, justifyContent: "center", padding: 20 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <Text style={{ marginBottom: 10, fontSize: 18 }}>Enter your name:</Text>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            borderRadius: 8,
+          }}
+          placeholder="Type here..."
+          value={name}
+          onChangeText={setName}
+        />
+        <Text>
+          Hello {name || 'Guest'}
+        </Text>
+      </KeyboardAvoidingView>
+
+      <View style={{ margin: 20 }}>
+        <Text style={{ marginBottom: 10, fontSize: 18 }}>Enter your password:</Text>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            borderRadius: 8,
+          }}
+          secureTextEntry
+          placeholder="Type password here..."
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+
+        <Text style={{ marginBottom: 10, fontSize: 18 }}>Enter your age:</Text>
+        <KeyboardAvoidingView
+      style={{flex: 1, justifyContent: "center", padding: 20 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'position'}
+      >
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            borderRadius: 8,
+          }}
+          placeholder="Type age here..."
+          keyboardType="numeric"
+          value={age}
+          onChangeText={setAge}
+        />
+      </KeyboardAvoidingView>
+        <Text>
+          You are {age} years old.
+        </Text>
+
+      <TextInput
+      multiline
+      numberOfLines={6}
+      placeholder="Type your bio...."
+      style={{
+        borderWidth: 1,
+        borderColor: '#000'
+      }}
+      />
+
+    </ScrollView>
+  </SafeAreaView>
   );
 }
 
